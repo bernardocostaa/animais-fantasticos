c9578f2ec7ae3e8b4a5144951160ce96dea31f02
Bernardo Costa <bernardo-v.f@hotmail.com>
2022-12-19 02:09:17 -0300

    refatorado fetch bitcoin

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,8 +1,8 @@
-export default function iniciarFetchBitcoin(){
-  fetch('https://blockchain.info/ticker').then((res)=>{
+export default function fetchBitcoin(url,itemTarget){
+  fetch(url).then((res)=>{
     res.json().then(body =>{
       console.log(body.BRL.sell);
-      const btcPreco = document.querySelector('.btc-preco')
+      const btcPreco = document.querySelector(itemTarget)
       btcPreco.innerText = (1000 / body.BRL.sell).toFixed(4)
     }).catch(erro =>{
       console.log(Error(erro));
